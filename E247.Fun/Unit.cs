1a00e4d1d3048a00dc58cd49c529e6dae41c52b5
Adam Guest <adam.guest@247e.com>
2016-05-27 15:30:07 +0200

    Update namespace reference in unit comments

--- E247.Fun/Unit.cs
+++ E247.Fun/Unit.cs
@@ -26,12 +26,12 @@ namespace E247.Fun
         public static bool operator !=(Unit lhs, Unit rhs) =>
             false;
 
-        // with using static E247.Juke.Model.Entities.Unit, allows using unit instead of the ugly Unit.Value
+        // with using static E247.Fun.Unit, allows using unit instead of the ugly Unit.Value
         // ReSharper disable once InconsistentNaming
         public static Unit unit =>
             Value;
 
-        // with using static E247.Juke.Model.Entities.Unit, allows using ignore(anything) to have anything return unit
+        // with using static E247.Fun.Unit, allows using ignore(anything) to have anything return unit
         // ReSharper disable once InconsistentNaming
         public static Unit ignore<T>(T anything) =>
             unit;
